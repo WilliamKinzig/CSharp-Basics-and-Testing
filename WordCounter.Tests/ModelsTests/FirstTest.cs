c0c7d02aec2392b36633edd9b3dc6afc218692b2
WilliamKinzig <wippawill2@gmail.com>
2018-06-29 14:37:31 -0700

    first commit, folder structure complete, basic test working, README.md file included

---
+++ WordCounter.Tests/ModelsTests/FirstTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class FirstTest
+  {
+    [TestMethod]
+    public void IsFirst_NumberDivisibleByFour_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ WordCounter/Models/First.cs
@@ -0,0 +1,11 @@
+namespace WordCounter
+{
+  public class First
+  {
+    public bool IsLeapYear(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
